dfe56dd8bd5a96bbb96b9990f488dcbc4ebe2028
Jeffrey A. Robinson <jeffrey.robinson@microsoft.com>
2017-07-06 08:15:49 -0700

    Initial Push for Swagger generated PS Modules

---
+++ Generated/generate.ps1
@@ -0,0 +1,52 @@
+
+$ErrorActionPreference = "Stop";
+
+Set-Alias AutoRest.exe autorest.exe
+
+# Initialize the environment
+
+$output="./Generated"
+$tmp="./tmp"
+$conf="autorest.md"
+
+if( Test-Path $output ) {
+    Remove-Item $output -Recurse | Out-Null
+}
+mkdir $output | Out-Null
+
+if( Test-Path $tmp ) {
+    Remove-Item $tmp -Recurse | Out-Null
+}
+
+autorest $conf --verbose --debug
+
+# Create PowerShell
+
+Import-Module ..\Tools\PSSwagger\PSSwagger\PSSwagger.psd1 -Force
+
+$spec = (Resolve-Path ".\Artifacts\common.json").ToString()
+$path = (Resolve-Path $output).ToString()
+
+$param = @{
+    SwaggerSpecUri = $spec
+    Path = $path
+    Name = "StorageAdmin"
+    UseAzureCsharpGenerator = $true
+    DefaultCommandPrefix= "Azs"   
+}
+
+New-PSSwaggerModule @param
+
+# Generate Code
+
+Import-Module ..\Tools\PSSwagger\PSSwagger\PSSwagger.Common.Helpers -Force
+Import-Module ..\Tools\PSSwagger\PSSwagger\PSSwagger.Azure.Helpers -Force
+Import-Module "$($param.Path)\$($param.Name)" -Force
+Get-Command -Module $param.Name
+
+# Cleanup Envirornment
+
+if( [System.IO.File]::Exists($tmp) ) {
+    Remove-Item $tmp -Recurse
+}
+
